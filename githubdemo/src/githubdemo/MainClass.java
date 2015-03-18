28535d02f16fdf215782560f0bc159ef57c86d05
shehza-scs <shehzaiqbal@gmail.com>
2015-03-18 11:13:33 +0500

    new commit

--- githubdemo/src/githubdemo/MainClass.java
+++ githubdemo/src/githubdemo/MainClass.java
@@ -5,6 +5,12 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+print();
+	}
+	
+	public static void print(){
+		
+		System.out.println("abc");
 	}
 
 }
